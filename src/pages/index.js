4aa1a92151e0e543c7c03e994244ca4c44e7b7e6
coderhawk999 <afroz.cs17@sahyadri.edu.in>
2020-09-17 15:11:37 +0530

    Added about section and cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import Layout from "../components/layout"
 import "../styles/main.scss"
 import Landing from "../components/landingPage/landing"
+import About from "../components/about/about"
 const IndexPage = () => {
   return (
     <Layout>
       <Landing />
+      <About />
     </Layout>
   )
 }
